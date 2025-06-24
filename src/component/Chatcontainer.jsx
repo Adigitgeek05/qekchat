03077fb6463e3c885fe41f47378b45fa90c7cb1d
Adigitgeek05 <adigitgeek@gmail.com>
2025-06-24 09:56:49 +0530

    Second commit

---
+++ src/component/Chatcontainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chatcontainer = () => {
+  return (
+    <div>
+      <h1>ChatContainer</h1>
+    </div>
+  )
+}
+
+export default Chatcontainer
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login to Qekchat</h1>
+      
+    </div>
+  )
+}
+
+export default LoginPage
