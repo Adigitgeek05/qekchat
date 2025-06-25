4b3a40c11fd1678ed1ad9a789cf34cfb5409e5fb
Adigitgeek05 <adigitgeek@gmail.com>
2025-06-25 06:35:28 +0530

    Sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -3,6 +3,8 @@ import logo from '../assets/logo.png'
 import { useNavigate } from 'react-router-dom'
 import menu_icon from '../assets/menu_icon.png'
 import search_icon from '../assets/search_icon.png'
+import { userDummyData } from '../assets/assets.js'
+import avatar_icon from '../assets/avatar_icon.png'
 
 
 const Sidebar = ({selectedUser,setselectedUser}) => {
@@ -33,6 +35,28 @@ const Sidebar = ({selectedUser,setselectedUser}) => {
         <input type="text" placeholder='Search User...' className='bg-transparent
         border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' />
       </div>
+    </div>
+    <div className='flex flex-col '>
+      {userDummyData.map((user,index) => (
+        <div onClick={() => {setselectedUser(user)}} 
+        key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded 
+        cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50' }`}>
+            <img src={user?.profilePic || avatar_icon} alt=""
+            className='w-[35px] aspect-[1/1] rounded-full ' />
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p> 
+              {
+                index< 3
+                ? <span className='text-green-400 text-xs'>Online</span>
+                : <span className='text-neutral-400 text-xs'>Offline</span>
+              }
+            </div>
+            {index >2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 
+            flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>}
+
+        </div>
+      ))}
+
     </div>
     </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,9 +12,9 @@ const HomePage = () => {
     <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
     overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <Sidebar />
-        <Chatcontainer />
-        <RightSidebar />
+        <Sidebar selectedUser={selectedUser} setselectedUser={setselectedUser} />
+        <Chatcontainer selectedUser={selectedUser} setselectedUser={setselectedUser} />
+        <RightSidebar selectedUser={selectedUser} setselectedUser={setselectedUser} />
     </div>
     </div> 
   )
